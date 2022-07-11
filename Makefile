69fc5f7c6c5cd7dad7fe11c75197f651bdc1c3bd
Sergio Hernandez <contact.sergiohernandez@gmail.com>
2022-07-11 20:40:08 +0100

    Update color script + tmux config

--- Makefile
+++ Makefile
@@ -2,3 +2,6 @@
 
 install: ## Install and configure the environment
 	@bash scripts/install.sh
+
+update:
+	@bash scripts/update_dotfiles.sh
---
+++ scripts/update_dotfiles.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# vim: ai:ts=4:sw=4:noet
+# Install SH development environment
+# Supports MacOS and Arch Linux
+
+cp ~/.bashrc ./
+cp ~/.nuxtrc ./
+cp ~/.profile ./
+cp ~/.tmux.conf ./
+cp ~/.zprofile ./
+cp ~/.zshenv ./
+cp ~/.zshrc ./
+
+# Remove all content before copying
+rm -r ./.config/alacritty
+cp -r ~/.config/alacritty ./.config
+rm -r ./.config/htop
+cp -r ~/.config/htop ./.config
+rm -r ./.config/mprocs
+cp -r ~/.config/mprocs ./.config
+cp -r ~/.config/mprocs ./.config
+
+# Do not remove all content before copying
+cp ~/scripts/* ./scripts
+
+# Sensitive (do not use *)
+cp ~/.config/gh/config.yml ./.config/gh
+cp ~/.config/git/config ./.config/git
+cp ~/.config/git/.gitignore_global ./.config/git
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    cp ~/.Xresources ./
+
+	rm -r ./.config/.Xresources.d/colorscheme
+    cp -r ~/.config/.Xresources.d/colorscheme ./.Xresources.d
+	rm -r ./.config/awesome
+    cp -r ~/.config/awesome ./.config
+	rm -r ./.config/picom
+    cp -r ~/.config/picom ./.config
+	rm -r ./.config/ranger
+    cp -r ~/.config/ranger ./.config
+	rm -r ./.config/rofi
+    cp -r ~/.config/rofi ./.config
+fi
